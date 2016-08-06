bb040860ba14acaf0e7664254065aafac6cb8870
Dean Friedland <dfmmalaw@gmail.com>
2016-08-06 16:13:26 -0400

    initial commit with variables declared and initialized

---
+++ BegC-ch2-datatypes/BegC-ch2-datatypes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BegC_ch2_datatypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variable declerations and initialization (assignment)
+            int venusMoonCount = 0;
+            float windSpeed = 450.0f;
+            string venusFact = "Venus is the only planet " + "that rotates clockwise";
+            // display the variable values
+            Console.WriteLine(venusFact);
+            Console.Write("Total moons = ");
+            Console.WriteLine(venusMoonCount);
+
+            Console.Write("Venusian windspeed = ");
+
+            Console.WriteLine(windSpeed + " miles/hour.");
+
+            Console.ReadLine();
+        }
+    }
+}
